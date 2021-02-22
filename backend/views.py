b5c8bd0c4f1b36ee3e933f3722c30ccf199313f2
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-22 22:53:49 +0300

    initial commit

---
+++ backend/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'backend/index.html')
+
+
+def about(request):
+    return render(request, 'backend/about.html')
+
+
+def services(request):
+    return render(request, 'backend/products.html')
+
+
+def store(request):
+    return render(request, 'backend/store.html')
